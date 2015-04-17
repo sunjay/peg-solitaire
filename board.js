82e4648ff61b3feca1bb4d70296b9117b3efbfe8
Sunjay Varma <varma.sunjay@gmail.com>
2015-04-17 01:29:28 -0400

    Made it so that you can only move if you take away a peg with each move

--- board.js
+++ board.js
@@ -35,6 +35,43 @@
         return this.tiles[0].length;
     };
 
+    Board.prototype.getTilesBetween = function(row, col, dest_row, dest_col) {
+        // For the two tiles to be orthogonal, either the rows or columns must be the same
+        if (row !== dest_row && col !== dest_col) {
+            throw new Error("Tiles must be orthogonal");
+        }
+
+        var tiles = [];
+
+        // Only one of these loops should actually run 
+
+        for (var i_row = Math.min(row, dest_row) + 1, d_row = Math.max(row, dest_row); i_row < d_row; i_row++) {
+            tiles.push([i_row, col]);
+        }
+
+        for (var i_col = Math.min(col, dest_col) + 1, d_col = Math.max(col, dest_col); i_col < d_col; i_col++) {
+            tiles.push([row, i_col]);
+        }
+        
+        return tiles;
+    };
+
+    Board.prototype.isFilledBetween = function(row, col, dest_row, dest_col) {
+        // Returns true if all tiles between (but not including either provided coordinates) are filled
+        var is_filled = true;
+        this.getTilesBetween(row, col, dest_row, dest_col).forEach(function(tile) {
+            var tile_row = tile[0];
+            var tile_col = tile[1];
+
+            if (this.tiles[tile_row][tile_col] !== TILES.FILLED) {
+                is_filled = false;
+                return false;
+            }
+        }.bind(this));
+
+        return is_filled;
+    };
+
     Board.prototype.getMovesAroundTile = function(row, col) {
         var tile = this.tiles[row][col];
         if (tile !== TILES.FILLED) {
@@ -52,7 +89,7 @@
             // vertical offset
             var i_row = row + offset;
             if (i_row >= 0 && i_row < this.height()) {
-                if (this.tiles[i_row][col] === TILES.EMPTY) {
+                if (this.tiles[i_row][col] === TILES.EMPTY && this.isFilledBetween(row, col, i_row, col)) {
                     move_tiles.push([i_row, col]);
                 }
             }
@@ -60,7 +97,7 @@
             // horizontal offset
             var i_col = col + offset;
             if (i_col >= 0 && i_col < this.width()) {
-                if (this.tiles[row][i_col] === TILES.EMPTY) {
+                if (this.tiles[row][i_col] === TILES.EMPTY && this.isFilledBetween(row, col, row, i_col)) {
                     move_tiles.push([row, i_col]);
                 }
             }
@@ -81,31 +118,21 @@
         if (this.tiles[dest_row][dest_col] !== TILES.EMPTY) {
             throw new Error("Cannot place tile on space that is not empty");
         }
-        
-        // For the two tiles to be orthogonal, either the rows or columns must be the same
-        if (row !== dest_row && col !== dest_col) {
-            throw new Error("Tiles must be orthogonal");
-        }
 
         // Note that this code does not explicitly enforce the "only one tile" between rule of peg solitaire. That responsibility is left up to the view layer. The view layer should respect the getMovesAroundTile() function.
 
         // Move the peg out of where it is now
         this.tiles[row][col] = TILES.EMPTY;
-        
-        // Only one of these loops should actually run an interation
-        for (var i_row = Math.min(row, dest_row) + 1, d_row = Math.max(row, dest_row); i_row < d_row; i_row++) {
-            // This guard ensures that BLANK tiles are not accidentally changed (though this should never happen)
-            if (this.tiles[i_row][col] === TILES.FILLED) {
-                this.tiles[i_row][col] = TILES.EMPTY;
-            }
-        }
 
-        for (var i_col = Math.min(col, dest_col) + 1, d_col = Math.max(col, dest_col); i_col < d_col; i_col++) {
+        this.getTilesBetween(row, col, dest_row, dest_col).forEach(function(tile) {
+            var tile_row = tile[0];
+            var tile_col = tile[1];
+
             // This guard ensures that BLANK tiles are not accidentally changed (though this should never happen)
-            if (this.tiles[row][i_col] === TILES.FILLED) {
-                this.tiles[row][i_col] = TILES.EMPTY;
+            if (this.tiles[tile_row][tile_col] === TILES.FILLED) {
+                this.tiles[tile_row][tile_col] = TILES.EMPTY;
             }
-        }
+        }.bind(this));
 
         // Fill in the destination tile
         this.tiles[dest_row][dest_col] = TILES.FILLED;
